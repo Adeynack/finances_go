546ad2f6894631f3cb4754f33bd276382c1cbe1d
Adeynack (David) <Adeynack@users.noreply.github.com>
2025-09-06 00:34:05 +0200

    query hook

--- pkg/app/database.go
+++ pkg/app/database.go
@@ -1,13 +1,17 @@
 package app
 
 import (
+	"context"
 	"database/sql"
 	"errors"
+	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
+	slogctx "github.com/veqryn/slog-context"
 )
 
 func MustConnectDatabase() *bun.DB {
@@ -18,6 +22,25 @@ func MustConnectDatabase() *bun.DB {
 
 	db := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
 	bunDB := bun.NewDB(db, pgdialect.New())
+	bunDB.AddQueryHook(&QueryHook{})
 
 	return bunDB
+}
+
+type QueryHook struct{}
+
+func (h *QueryHook) BeforeQuery(ctx context.Context, event *bun.QueryEvent) context.Context {
+	switch event.Operation() {
+	case "INSERT", "UPDATE":
+		slogctx.Info(ctx, "Saving model",
+			slog.String("model_type", fmt.Sprintf("%T", event.Model.Value())),
+			slog.Any("model", event.Model.Value()),
+		)
+	}
+
+	return ctx
+}
+
+func (h *QueryHook) AfterQuery(ctx context.Context, event *bun.QueryEvent) {
+	// fmt.Println(time.Since(event.StartTime), string(event.Query))
 }
